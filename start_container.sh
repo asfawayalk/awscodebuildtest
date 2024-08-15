e0e2bd12846cc4f6976e41d9d8de83f2c85ddb4c
asfaw ayalkibet <asfaw.ayalkibet@gmail.com>
2024-08-15 16:23:25 +0300

    test change

--- start_container.sh
+++ start_container.sh
@@ -4,6 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull asfawayalk/simple-python-flask-app
 
-
 # Run the Docker image as a container 
 docker run -d -p 5000:5000 asfawayalk/simple-python-flask-app
